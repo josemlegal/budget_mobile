6825a06e71c7caff46e2db75a825cef0824fdfd3
josemlegal <josemlegal97@gmail.com>
2023-03-18 23:23:23 -0300

    Add the bottom navigation bar

--- lib/presentation/widgets/bottom_navigation_bar.dart
+++ lib/presentation/widgets/bottom_navigation_bar.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 
+class BottomNavigationBar extends StatelessWidget {
+  const BottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    destinations = [
+      NavigationDestination(
+        icon: const Icon(Icons.home),
+        label: 'Home',
+        route: '/',
+      ),
+      NavigationDestination(
+        icon: const Icon(Icons.list),
+        label: 'Transactions',
+        route: '/transactions',
+      ),
+      NavigationDestination(
+        icon: const Icon(Icons.person),
+        label: 'Profile',
+        route: '/profile',
+      ),
+    ];
+    return NavigationBar(destinations: destinations);
+  }
+}
