32d35bdb6f8e1a2005d28f883676f0bb2aecebb4
josemlegal <josemlegal97@gmail.com>
2023-04-04 13:30:33 -0400

    Add the locator

---
+++ lib/core/dependency_injection/locator.dart
@@ -0,0 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+final GetIt locator = GetIt.instance;
+
+void setupLocator() {
+  locator.registerLazySingleton<SnackbarService>(
+    () => SnackbarService(),
+  );
+  locator.registerLazySingleton<DialogService>(
+    () => DialogService(),
+  );
+}
