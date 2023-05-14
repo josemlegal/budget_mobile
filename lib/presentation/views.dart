c9665ac7067e6696d3013906261ae3503048ea3c
josemlegal <josemlegal97@gmail.com>
2023-05-13 23:08:26 -0400

    Delete unnecesary views and add barell

---
+++ lib/presentation/views.dart
@@ -0,0 +1,4 @@
+export 'package:budgetkp/domain/transactions/models/transactions.dart';
+export 'package:budgetkp/presentation/auth/views/login_view.dart';
+export 'package:budgetkp/presentation/auth/views/onboarding_view.dart';
+export 'package:budgetkp/presentation/settings/views/settings_view.dart';
