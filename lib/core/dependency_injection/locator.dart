31067b196d42eb96d1c72fe699b8109ba7879f71
josemlegal <josemlegal97@gmail.com>
2023-05-13 23:06:01 -0400

    Update the locator

--- lib/core/dependency_injection/locator.dart
+++ lib/core/dependency_injection/locator.dart
@@ -1,16 +1,16 @@
+import 'package:budgetkp/core/services/http_service.dart';
 import 'package:get_it/get_it.dart';
+
 import 'package:stacked_services/stacked_services.dart';
 
 final GetIt locator = GetIt.instance;
 
 void setupLocator() {
+  locator.registerLazySingleton<HttpService>(
+    () => HttpService(),
+  );
+
   locator.registerLazySingleton<SnackbarService>(
     () => SnackbarService(),
-  );
-  locator.registerLazySingleton<DialogService>(
-    () => DialogService(),
-  );
-  locator.registerLazySingleton(
-    () => NavigationService(),
   );
 }
