a8615f1bb0349502c4ebdd69bf16d28e6b0abd95
josemlegal <josemlegal97@gmail.com>
2023-05-13 23:06:26 -0400

    Improve app theme

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,130 +1,72 @@
 import 'package:flutter/material.dart';
 
-class AppColors {
-  static const primary = Color(0xFF96D2DC);
-  static const primaryLight = Color(0xFFCAE8ED);
-  static const primaryDark = Color(0xFF6A9BA4);
-  static const accent = Color(0xFF323C50);
-  static const accentLight = Color(0xFF989DA7);
-  static const accentDark = Color(0xFF11151E);
-  static const warning = Color(0xFFFAC850);
-  static const error = Color(0xFFFD5D5D);
-  static const primaryTextColor = Color(0xFF092C4C);
-  static const darkGrey = Color(0xFF7D7D96);
-  static const grey = Color(0xFFB7BEC7);
-  static const neutral = Color(0xFFD5D5E1);
-  static const neutralLight = Color(0xFFF4F4F9);
-  static const black = Color(0xFF000000);
-}
+const colorList = <Color>[
+  Color(0xFF00C691),
+  Colors.teal,
+  Colors.blue,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink,
+  Colors.pinkAccent,
+];
 
-const TextStyle headline9 = TextStyle(
-  fontFamily: 'Campton',
-  fontSize: 16,
-  height: 1,
-  fontWeight: FontWeight.w600,
-  color: AppColors.black,
-);
+class AppTheme {
+  final int selectedColor;
+  final bool isDarkMode;
 
-const TextStyle headline10 = TextStyle(
-  fontFamily: 'Campton',
-  fontSize: 14,
-  height: 1,
-  fontWeight: FontWeight.w600,
-  color: AppColors.black,
-);
+  AppTheme({this.isDarkMode = true, this.selectedColor = 0})
+      : assert(selectedColor >= 0, 'Selected color must be greater then 0'),
+        assert(selectedColor < colorList.length,
+            'Selected color must be less or equal than ${colorList.length - 1}');
 
-class Themes {
-  static final ThemeData light = ThemeData(
-    scaffoldBackgroundColor: AppColors.neutralLight,
-    primaryColor: AppColors.primary,
-    colorScheme: const ColorScheme.light(
-        primary: AppColors.primary, secondary: AppColors.accent),
-    textTheme: const TextTheme(
-      displayLarge: TextStyle(
-        fontFamily: 'Campton',
-        fontSize: 52,
-        height: 1,
-        fontWeight: FontWeight.w600,
-        color: AppColors.black,
-      ),
-      displayMedium: TextStyle(
-        fontFamily: 'Campton',
-        fontSize: 46,
-        height: 1,
-        fontWeight: FontWeight.w600,
-        color: AppColors.black,
-      ),
-      displaySmall: TextStyle(
-        fontFamily: 'Campton',
-        fontSize: 40,
-        height: 1,
-        fontWeight: FontWeight.w600,
-        color: AppColors.black,
-      ),
-      headlineMedium: TextStyle(
-        fontFamily: 'Campton',
-        fontSize: 32,
-        height: 1.1,
-        fontWeight: FontWeight.w600,
-        color: AppColors.black,
-      ),
-      headlineSmall: TextStyle(
-        fontFamily: 'Campton',
-        fontSize: 27,
-        height: 0.9,
-        fontWeight: FontWeight.w600,
-        color: AppColors.black,
-      ),
-      titleLarge: TextStyle(
-        fontFamily: 'Campton',
-        fontSize: 24,
-        height: 1,
-        fontWeight: FontWeight.w600,
-        color: AppColors.black,
-      ),
-      titleMedium: TextStyle(
-        fontFamily: 'Campton',
-        fontSize: 20,
-        height: 1,
-        fontWeight: FontWeight.w600,
-        color: AppColors.black,
-      ),
-      titleSmall: TextStyle(
-        fontFamily: 'Campton',
-        fontSize: 18,
-        height: 1,
-        fontWeight: FontWeight.w600,
-        color: AppColors.black,
-      ),
-      bodyLarge: TextStyle(
-        fontFamily: 'NotoSans',
-        fontSize: 18,
-        height: 1,
-        fontWeight: FontWeight.normal,
-        color: AppColors.black,
-      ),
-      bodyMedium: TextStyle(
-        fontFamily: 'NotoSans',
-        fontSize: 16,
-        height: 1.4,
-        fontWeight: FontWeight.normal,
-        color: AppColors.black,
-      ),
-      bodySmall: TextStyle(
-        fontFamily: 'NotoSans',
-        fontSize: 14,
-        height: 1.4,
-        fontWeight: FontWeight.normal,
-        color: AppColors.black,
-      ),
-      labelSmall: TextStyle(
-        fontFamily: 'NotoSans',
-        fontSize: 12,
-        height: 1.4,
-        letterSpacing: 1.1,
-        fontWeight: FontWeight.w400,
-        color: AppColors.black,
-      ),
-    ),
-  );
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorList[selectedColor],
+        brightness: isDarkMode ? Brightness.dark : Brightness.light,
+        appBarTheme: const AppBarTheme(
+          centerTitle: false,
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontFamily: 'Campton',
+            fontSize: 40,
+            fontWeight: FontWeight.w600,
+          ),
+          displayMedium: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+          displaySmall: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+          headlineMedium: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+          headlineSmall: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+          ),
+          titleLarge: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w600,
+          ),
+          bodyLarge: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.normal,
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      );
+
+  AppTheme copyWith({int? selectedColor, bool? isDarkMode}) => AppTheme(
+        selectedColor: selectedColor ?? this.selectedColor,
+        isDarkMode: isDarkMode ?? this.isDarkMode,
+      );
 }
