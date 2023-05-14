a18bd508041fc38eb7fb6e59604d36eceac532d1
josemlegal <josemlegal97@gmail.com>
2023-05-13 23:08:01 -0400

    Add the settings view and controller

---
+++ lib/presentation/settings/views/settings_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SettingsView extends StatelessWidget {
+  const SettingsView({super.key});
+
+  static const name = '/settings-view';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
